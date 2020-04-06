b6372247d39e9934f19728eb6f42cd688f1b2221
LukeN <luke.nizik@gmail.com>
2020-04-06 16:13:16 +0200

    Audio player plays track

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,16 +1,29 @@
 import React, { useState, useContext } from 'react';
 import PlayButton from './PlayButton';
 import { AudioUtilsContext } from 'common/AudioUtils';
+import AudioTrackPlayer from 'common/AudioTrackPlayer';
+import { isUndefined } from 'util';
 
-import click1 from 'assets/click1.wav';
+let player: AudioTrackPlayer;
 
 function AudioPlayer() {
   const [ isPlaying, togglePlaying ] = useState(false);
   const audioUtils = useContext(AudioUtilsContext);
+
+
   const handleTogglePlaying = () => {
-    audioUtils.loadClip(click1).then(clip => {
-      clip.start();
-    });
+    if (isUndefined(player)) {
+      player = new AudioTrackPlayer(audioUtils);
+      console.log("Player created");
+    }
+
+    if (isPlaying) {
+      player.stop();
+    }
+    else {
+      player.start();
+    }
+
     togglePlaying(!isPlaying);
   };
 
