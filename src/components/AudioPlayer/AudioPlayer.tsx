ce6abaf7bfe2eafe644b4a392d36e47b355eaf5e
LukeN <luke.nizik@gmail.com>
2020-04-09 16:16:55 +0200

    Audio track GUI integration

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -1,27 +1,39 @@
 import React, { useState, useContext } from 'react';
+import { connect } from 'react-redux';
 import PlayButton from './PlayButton';
 import { AudioUtilsContext } from 'common/AudioUtils';
 import AudioTrackPlayer from 'common/AudioTrackPlayer';
 import { isUndefined } from 'util';
+import { RootState } from 'state'; // eslint-disable-line no-unused-vars
+import { DrumSettingsState } from 'state/types'; // eslint-disable-line no-unused-vars
 
 let player: AudioTrackPlayer;
 
-function AudioPlayer() {
+const updatePlayer = (settings: DrumSettingsState) => {
+  const gen = player.generator;
+  gen.bpm = settings.tempo;
+}
+
+type Props = ReturnType<typeof mapState>
+
+function AudioPlayer(props: Props) {
+  const { settings } = props;
   const [ isPlaying, togglePlaying ] = useState(false);
   const audioUtils = useContext(AudioUtilsContext);
 
+  player && updatePlayer(settings);
 
   const handleTogglePlaying = () => {
     if (isUndefined(player)) {
       player = new AudioTrackPlayer(audioUtils);
-      console.log("Player created");
+
     }
 
-    if (isPlaying) {
-      player.stop();
+    if (!isPlaying) {
+      player.start();
     }
     else {
-      player.start();
+      player.stop();
     }
 
     togglePlaying(!isPlaying);
@@ -32,4 +44,8 @@ function AudioPlayer() {
   );
 }
 
-export default AudioPlayer;
+const mapState = (state: RootState) => {
+  return { settings: state.drumSettings };
+}
+
+export default connect(mapState)(AudioPlayer);
--- src/components/DrumSettings/DrumSettings.test.tsx
+++ src/components/DrumSettings/DrumSettings.test.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ShallowWrapper } from 'enzyme'; // eslint-disable-line no-unused-vars
-
 import { expect } from 'chai';
 
-import DrumSettings from './';
+import { DrumSettings } from './DrumSettings';
+
+import { updateTempo } from 'state/actions/drumSettingsActions';
 
 import TempoEdit from './TempoEdit';
 import PhraseLengthEdit from './PhraseLengthEdit';
 
-let wrapper: ShallowWrapper;
+let wrapper: ShallowWrapper | null;
 
 describe('Drum settings', () => {
   beforeAll(() => {
-    wrapper = shallow((<DrumSettings/>));
+    const props = {
+      settings: {
+        tempo: 1
+      },
+      updateTempo: updateTempo
+    };
+
+    wrapper = shallow(<DrumSettings {...props}/>);
+  });
+
+  afterAll(() => {
+    wrapper = null;
   });
 
   it("draws tempo edit", () => {
-    expect(wrapper.find(TempoEdit)).to.have.lengthOf(1);
+    expect(wrapper!.find(TempoEdit)).to.have.lengthOf(1);
   });
 
   it("draws phrase length edit", () => {
-    expect(wrapper.find(PhraseLengthEdit)).to.have.lengthOf(1);
+    expect(wrapper!.find(PhraseLengthEdit)).to.have.lengthOf(1);
   });
 });
