b4a6f0b71923c1aa2d20c0a4f43595a09d5e790c
LukeN <luke.nizik@gmail.com>
2020-04-09 16:15:00 +0200

    create redux store

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import * as serviceWorker from 'serviceWorker';
 import AudioUtils, { AudioUtilsProvider } from 'common/AudioUtils';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from 'state';
 
 const audioUtils = new AudioUtils();
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <AudioUtilsProvider value={audioUtils}>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
-  </AudioUtilsProvider>,
+  <Provider store={store}>
+    <AudioUtilsProvider value={audioUtils}>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    </AudioUtilsProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
