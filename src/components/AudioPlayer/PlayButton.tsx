b7b8872b4fe23d011fe700e4b8479debbbab60a2
LukeN <luke.nizik@gmail.com>
2020-04-10 16:00:50 +0200

    press space - text

--- src/components/AudioPlayer/PlayButton.tsx
+++ src/components/AudioPlayer/PlayButton.tsx
@@ -4,6 +4,7 @@ import IconButton from '@material-ui/core/IconButton';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   icon: {
@@ -25,9 +26,12 @@ function PlayButton(props: Props) {
   };
 
   return (
-    <IconButton onClick={handleOnClick} className={classes.icon}>
-      {isPlaying ? <PauseIcon fontSize="inherit"/> : <PlayArrowIcon fontSize="inherit"/> }
-    </IconButton>
+    <React.Fragment>
+      <IconButton onClick={handleOnClick} className={classes.icon}>
+        {isPlaying ? <PauseIcon fontSize="inherit"/> : <PlayArrowIcon fontSize="inherit"/> }
+      </IconButton>
+      <Typography>...press SPACE</Typography>
+    </React.Fragment>
   );
 }
 
