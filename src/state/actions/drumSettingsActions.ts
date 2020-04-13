5f87ac9858335e3460454ded5e3581d12e180e42
LukeN <luke.nizik@gmail.com>
2020-04-13 20:20:29 +0200

    Added analytics events to redux

--- src/state/actions/drumSettingsActions.ts
+++ src/state/actions/drumSettingsActions.ts
@@ -2,6 +2,10 @@ import {
   UPDATE_TEMPO,
   UPDATE_BEAT_OFFSET,
   UPDATE_TEMPO_OSCILATION,
+  COMMIT_TEMPO,
+  COMMIT_BEAT_OFFSET,
+  COMMIT_TEMPO_OSCILATION,
+  TOGGLE_PLAYBACK,
   SettingsActionTypes, // eslint-disable-line no-unused-vars
 } from 'state/types';
 
@@ -23,5 +27,33 @@ export function updateTempoOscilation(tempo: number): SettingsActionTypes {
   return { 
     type: UPDATE_TEMPO_OSCILATION,
     payload: tempo
+  }
+}
+
+export function commitTempo(tempo: number): SettingsActionTypes {
+  return {
+    type: COMMIT_TEMPO,
+    payload: tempo
+  }
+}
+
+export function commitBeatOffset(offset: number): SettingsActionTypes {
+  return {
+    type: COMMIT_BEAT_OFFSET,
+    payload: offset
+  }
+}
+
+export function commitTempoOscilation(tempo: number): SettingsActionTypes {
+  return {
+    type: COMMIT_TEMPO_OSCILATION,
+    payload: tempo
+  }
+}
+
+export function togglePlayback(enabled: boolean): SettingsActionTypes {
+  return { 
+    type: TOGGLE_PLAYBACK,
+    payload: enabled
   }
 }
--- src/state/types.ts
+++ src/state/types.ts
@@ -8,6 +8,12 @@ export const UPDATE_TEMPO = 'UPDATE_TEMPO';
 export const UPDATE_BEAT_OFFSET = 'UPDATE_BEAT_OFFSET';
 export const UPDATE_TEMPO_OSCILATION = 'UPDATE_TEMPO_OSCILATION';
 
+export const COMMIT_TEMPO = 'COMMIT_TEMPO';
+export const COMMIT_BEAT_OFFSET = 'COMMIT_BEAT_OFFSET';
+export const COMMIT_TEMPO_OSCILATION = 'COMMIT_TEMPO_OSCILATION';
+
+export const TOGGLE_PLAYBACK = 'TOGGLE_PLAYBACK';
+
 interface UpdateTempoAction {
   type: typeof UPDATE_TEMPO,
   payload: number
@@ -23,4 +29,31 @@ interface UpdateTempoOscilation {
   payload: number
 }
 
-export type SettingsActionTypes = UpdateTempoAction | UpdateBeatOffsetAction | UpdateTempoOscilation;
+interface CommitTempo {
+  type: typeof COMMIT_TEMPO,
+  payload: number
+}
+
+interface CommitBeatOffset {
+  type: typeof COMMIT_BEAT_OFFSET,
+  payload: number
+}
+
+interface CommitTempoOscilation {
+  type: typeof COMMIT_TEMPO_OSCILATION,
+  payload: number
+}
+
+interface TogglePlayback {
+  type: typeof TOGGLE_PLAYBACK,
+  payload: boolean 
+}
+
+export type SettingsActionTypes = 
+  UpdateTempoAction | 
+  UpdateBeatOffsetAction | 
+  UpdateTempoOscilation |
+  CommitTempo |
+  CommitBeatOffset |
+  CommitTempoOscilation |
+  TogglePlayback;
